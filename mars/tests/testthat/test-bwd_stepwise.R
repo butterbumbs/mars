32e46333f8f807e9a7a611d6ea694969a9e82ed0
Zvikomborero Kennedy Jokonya <zjokonya@gmail.com>
2026-03-26 19:45:49 -0700

    Delete mars directory

--- mars/tests/testthat/test-bwd_stepwise.R
+++
@@ -1,6 +0,0 @@
-# testbwd_stepwise.R
-library(mars)
-load("testbwd_stepwise.RData")
-test_that("bwd_stepwise() returns the correct object", {
-  expect_equal(bwd_stepwise(testfwd,testmc), testbwd)
-})
