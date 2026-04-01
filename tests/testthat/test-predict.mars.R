0a1395a9af6d3b2902d80f7e3bbbf9cba6f3c934
Zvikomborero Kennedy Jokonya <zjokonya@gmail.com>
2026-04-01 02:52:11 -0700

    Delete tests directory

--- tests/testthat/test-predict.mars.R
+++
@@ -1,6 +0,0 @@
-# testpredict.mars.R
-library(mars)
-load("testpredict.RData")
-test_that("predict.mars() returns the correct object", {
-  expect_equal(predict.mars(testmars,newdata=marstestdata), testpredict)
-})
